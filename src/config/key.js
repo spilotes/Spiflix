ca40005b3730b6e87f371b1172819500b031090c
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-05-12 00:03:55 -0300

    Add files via upload

---
+++ src/config/key.js
@@ -0,0 +1,1 @@
+export const APIkey = "b96b3a9ca72e420bfee8fca8f438861d";
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./global.css";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
